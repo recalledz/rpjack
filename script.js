5c2fa1cb718d5a90cd9e3bcc37e345966a3f70f5
recalledz <f.froilanrivera@gmail.com>
2025-06-04 20:39:02 -0400

    Add death animation for dealer life bar

--- script.js
+++ script.js
@@ -414,8 +414,10 @@ function onDealerDefeat() {
   stageData.kills += 1;
   killsDisplay.textContent = `Kills: ${stageData.kills}`;
   dealerDeathAnimation();
-  nextStageChecker();
-  respawnDealerStage();
+  dealerBarDeathAnimation(() => {
+    nextStageChecker();
+    respawnDealerStage();
+  });
 } // need to define xp formula
 
 function onBossDefeat(boss) {
@@ -535,8 +537,21 @@ function cDealerDamage(damageAmount = null, ability = null, source = "dealer") {
     dCardPane.classList.add("dealer-dead")
 
     dCardWrapper.addEventListener("animationend", () => {
-       dCardContainer.innerHTML = "";
+      dCardContainer.innerHTML = "";
       renderDealerCard()
+    }, { once: true });
+  }
+
+  function dealerBarDeathAnimation(callback) {
+    const bar = document.querySelector(".dealerLifeContainer");
+    if (!bar) {
+      if (callback) callback();
+      return;
+    }
+    bar.classList.add("bar-dead");
+    bar.addEventListener("animationend", () => {
+      removeDealerLifeBar();
+      if (callback) callback();
     }, { once: true });
   }
 
