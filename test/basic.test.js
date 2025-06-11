71808b05a444bef8a8525457fa042c57051f2fbf
recalledz <f.froilanrivera@gmail.com>
2025-06-10 21:13:47 -0400

    Update basic.test.js

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,8 +1,10 @@
+import { expect } from 'chai';
+
 console.log("✅ basic.test.js is loaded!");
 
-// test/basic.test.js
 describe("🔥 Force Fail Test", () => {
   it("should fail if this is being run", () => {
-    chai.expect(true).to.equal(false);
+    expect(true).to.equal(false); // this is meant to fail on purpose
   });
 });
+
