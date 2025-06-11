d2b3612aeb8b47b2ee6c58e3eba3a3176f557094
recalledz <f.froilanrivera@gmail.com>
2025-06-10 21:42:13 -0400

    Update karma.conf.cjs

--- karma.conf.cjs
+++ karma.conf.cjs
@@ -1,30 +1,40 @@
-module.exports = function(config) {
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
+module.exports = function (config) {
   config.set({
     basePath: '',
     hostname: '127.0.0.1',
-    frameworks: ['mocha', 'chai', 'esm'],
+
+    frameworks: ['esm', 'mocha', 'chai'],
+
     files: [
       { pattern: 'test/**/*.test.js', type: 'module' },
       { pattern: '*.js', included: false, type: 'module' }
     ],
+
     esm: {
-      nodeResolve: true
+      nodeResolve: true,
+      preserveSymlinks: true,
+      compatibility: 'none'
     },
+
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox', '--disable-gpu']
       }
     },
+
     browsers: ['ChromeHeadlessNoSandbox'],
     singleRun: true,
     browserNoActivityTimeout: 30000,
     captureTimeout: 30000,
+
     plugins: [
+      require('karma-esm'),
       require('karma-mocha'),
       require('karma-chai'),
-      require('karma-chrome-launcher'),
-      require('@open-wc/karma-esm')
+      require('karma-chrome-launcher')
     ]
   });
 };
