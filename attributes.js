09da48670c48eb13b4f1269be2615604af1aeb1b
recalledz <f.froilanrivera@gmail.com>
2025-07-05 15:16:39 -0400

    Add Dexterity and Intelligence attributes

--- attributes.js
+++ attributes.js
@@ -19,6 +19,18 @@ export const attributes = {
     get hpBonus() {
       return 10 * this.points;
     }
+  },
+  Dexterity: {
+    points: 0,
+    get attackSpeedMultiplier() {
+      return 1 + 0.05 * this.points;
+    }
+  },
+  Intelligence: {
+    points: 0,
+    get constructPotencyMultiplier() {
+      return 1 + 0.03 * this.points;
+    }
   }
 };
 
@@ -27,7 +39,7 @@ export function addStrength(points = 1) {
 }
 
 export function strengthXpMultiplier(task) {
-  const affected = ['Woodcutting', 'Log Pine', 'Mining', 'Smithing'];
+  const affected = ['Log Pine', 'Mining', 'Smithing'];
   return affected.includes(task) ? 1 + attributes.Strength.points * 0.1 : 1;
 }
 
@@ -38,4 +50,22 @@ export function addEndurance(points = 1) {
 export function enduranceXpMultiplier(task) {
   const affected = ['Building', 'Defending', 'Combat'];
   return affected.includes(task) ? 1 + attributes.Endurance.points * 0.1 : 1;
+}
+
+export function addDexterity(points = 1) {
+  attributes.Dexterity.points += points;
+}
+
+export function dexterityXpMultiplier(task) {
+  const affected = ['Woodcutting', 'Gather Fruit'];
+  return affected.includes(task) ? 1 + attributes.Dexterity.points * 0.1 : 1;
+}
+
+export function addIntelligence(points = 1) {
+  attributes.Intelligence.points += points;
+}
+
+export function intelligenceXpMultiplier(task) {
+  const affected = ['Chant', 'Research'];
+  return affected.includes(task) ? 1 + attributes.Intelligence.points * 0.1 : 1;
 }
